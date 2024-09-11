82f2ebd73a225a437aa95cd716e74f5bf6c4193a
EdmundoVazquez-023 <jedmjul20@gmail.com>
2024-09-11 17:29:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "<p>Hola, Mundo siuuuuuuu!</p>"
-        <a href="/alumnos">
-        <button>Ir a Alumnos</button>
-    </a>
+       
     
 
 @app.route("/alumnos")
