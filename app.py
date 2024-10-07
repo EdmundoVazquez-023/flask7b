d4c4a4b1b676d19da924a849e387597fed24b4cf
EdmundoVazquez-023 <jedmjul20@gmail.com>
2024-10-07 17:15:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ def editar():
     id = request.args["id"]
 
     cursor = con.cursor(dictionary=True)
-    sql    = """"
+    sql    = """
     SELECT Id_Experiencia, Nombre_Apellido, Comentario, Calificacion FROM tst0_experiencias
     WHERE Id_Experiencia = %s
     """"
@@ -141,7 +141,7 @@ def eliminar():
     id = request.form["id"]
 
     cursor = con.cursor(dictionary=True)
-    sql    = """"
+    sql    = """
     DELETE FROM tst0_experiencias
     WHERE Id_Experiencia = %s
     """"
